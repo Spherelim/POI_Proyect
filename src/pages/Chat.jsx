7413f8bed4b9111ed510f1084e82f7129eac3a19
Spherelim <mau.ortiz350@gmail.com>
2026-03-04 22:13:25 -0600

    construction:Provar Vite+React en el proyecto

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,5 @@
+function Chat(){
+    return <h1> Chat MundiChat del Usuario </h1>
+}
+
+export default Chat
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { useNavigate } from "react-router-dom"
+
+function Home(){
+    const navigate = useNavigate()
+
+    return(
+        <div style={styles.container}>
+            <h1 style={styles.title}>MundiChat</h1>
+            <p style={styles.text}>
+                Conecta en Tiempo Real Con tus Amigos Aficionados.
+            </p>
+
+            <button style={styles.button} onClick={()=>navigate("/Chat")}>
+                Entrar al Chat
+            </button>
+
+        </div>
+    )
+}
+
+const styles ={
+    container:{
+        height: "100vh",
+        background: "#1e1f2f",
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: "3rem"
+    },
+    text: {
+        margin: "20px 0"
+    },
+    button: {
+        padding: "12px 24px",
+        fontSize: "1rem",
+        borderRadius: "8px",
+        border: "none",
+        cursor: "pointer"
+    }
+}
+
+export default Home
