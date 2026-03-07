6910075a45a04a90769029c7ab2f500d2355ffb8
RaulCortez0 <rulygamers02@gmail.com>
2026-03-06 22:40:37 -0600

    Home actualizado con mas archivos y diseño

--- src/components/Hero-Home.jsx
+++ src/components/Hero-Home.jsx
@@ -10,6 +10,15 @@ function Hero(){
             <div className="tagline">
                 Conecta a tiempo real con tus amigos aficionados
             </div>
+
+            <div className="dev">
+
+                <h2>DESARROLLADO POR:</h2>
+
+                <p>MAURICIO ELEUTERIO ORTIZ RODRIGUEZ (FRONTEND)</p>
+                <p>JOSE RAUL CORTEZ RICO (BACKEND)</p>
+                <p>MAURICIO ELEUTERIO ORTIZ RODRIGUEZ (BACKEND)</p>
+             </div>
             <Developers/>
             <StartButton/>
         </div>
