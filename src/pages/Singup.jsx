c21263023be7060cb6678edec2afdb386fb872e7
RaulCortez0 <rulygamers02@gmail.com>
2026-03-07 02:06:18 -0600

    Mejora en diseño de registro

--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -4,15 +4,29 @@ import "./Singup.css"
 function SingUp(){
     const navigate = useNavigate();
     return(
-        <div className="Sing">
-            <div className="Content">
-                <input type="text" placeholder="Nombre Completo" />
-                <input type="text" placeholder="Nombre de Usuario" />
-                <input type="email" placeholder="Correo Electronico" />
-                <input type="password" placeholder="Contraseña" />
-                <p>Fecha de Nacimiento</p>
-                <input type="date" placeholder="Fecha de Nacimiento" />
-                <button onClick={()=>navigate("/Chat")}>Register</button>
+
+         <div className="Image-Wrapper-Registro"> 
+
+            <div className= "Imagen-Fondo-Registro"></div>
+
+            <div className="Sing">
+                <div className="Content">
+                    
+                    <button 
+                        className="Btn-Cerrar" 
+                        onClick={() => navigate("/")}
+                        aria-label="Regresar a inicio">✕
+                    </button>
+
+                    <h1 className="Registrar">Registrar</h1>
+                    <input type="text" placeholder="Nombre Completo" />
+                    <input type="text" placeholder="Nombre de Usuario" />
+                    <input type="email" placeholder="Correo Electronico" />
+                    <input type="password" placeholder="Contraseña" />
+                    <p>Fecha de Nacimiento</p>
+                    <input type="date" placeholder="Fecha de Nacimiento" />
+                    <button className="Btn-Registrar" onClick={()=>navigate("/Chat")}>Register</button>
+                </div>
             </div>
         </div>
     )
